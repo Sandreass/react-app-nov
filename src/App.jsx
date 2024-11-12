09344ce29745957b468aa3f5a38ffa41196f70b9
Andrei Stroianetki <sandreasweb@gmail.com>
2024-11-12 20:34:05 +0100

    adding header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-    <h1>Counties...</h1>
-  </div>
+    <>
+    <Header />
+  </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <div>
+        <h1 className="text-2xl text-center">Counties...</h1>
+      </div>
+    );
+  };
+  
+  export default Header;
